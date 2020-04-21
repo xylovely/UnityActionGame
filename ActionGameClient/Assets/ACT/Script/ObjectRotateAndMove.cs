01c0f06306e80033239d5a8ecdc1f78218f3e096
xy <190187778@qq.com>
2020-04-21 17:28:30 +0800

    更新

---
+++ ActionGameClient/Assets/ACT/Script/ObjectRotateAndMove.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotateAndMove : MonoBehaviour {
+
+    [Header("是否开启自转")]
+    public bool isRotateSelf = false;
+    
+    [Header("定义自转的速度")]
+    public float _RotationSpeed = 0.1f; //定义自转的速度
+
+    [Header("是否开启公转")]
+    public bool isRotateAround = false;
+
+    [Header("物体需要公转的参照物")]
+    public GameObject Axis; //物体需要公转的参照物
+
+    [Header("公转速度")]
+    public float _PublicRotationSpeed = 0.1f; //公转速度
+
+    [Header("是否开启上下浮动")]
+    public bool isUpDown = false;
+
+    [Header("上下移动速度")]
+    public float upDownSpeed = 1f;
+
+    [Header("向上移动的范围")]
+    public float upArea = 5f;
+
+    [Header("向下移动的范围")]
+    public float downArea = -5f;
+
+    private bool upOrDown = true;
+
+    private Vector3 oriPosition;
+    // Use this for initialization
+    void Start () {
+        oriPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isRotateSelf)
+        {
+            transform.Rotate(Vector3.down * _RotationSpeed, Space.World); //物体自转
+        }
+        
+
+        if (isUpDown)
+        {
+            if (transform.position.y >= oriPosition.y + upArea)
+            {
+                upOrDown = false;
+                
+            }
+            else if (transform.position.y <= oriPosition.y + downArea)
+            {
+                upOrDown = true;
+                
+            }
+            if (upOrDown)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.down * Time.deltaTime);
+            }
+        }
+
+        if (isRotateAround)
+        {
+            transform.RotateAround(Axis.transform.position, Vector3.up, _PublicRotationSpeed);//将需要公转的参照物拖入，设置公转
+        }
+        
+    }
+}
---
+++ ActionGameClient/Assets/ACT/Shadow/ShadowCamera.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace MyLib
+{
+    public class ShadowCamera : MonoBehaviour
+    {
+        public GameObject player;
+
+        public Vector3 CamPos;
+        private Shader shader;
+        
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            GetComponent<Camera>().enabled = false;
+            shader = Shader.Find("Custom/shadowMap2");
+        }
+
+        private List<GameObject> objectList;
+        private void Start()
+        {
+            objectList = new List<GameObject>();
+
+            foreach (Transform t in player.transform)
+            {
+                if (t.GetComponent<Renderer>() != null && t.name != "playerLight(Clone)")
+                {
+                    objectList.Add(t.gameObject);
+                    //t.gameObject.layer = (int)GameLayer.ShadowMap;
+                }
+            }
+        }
+
+        private void SetObjectLayer(int layer)
+        {
+            for (int i = 0; i < objectList.Count; i++)
+            {
+                objectList[i].layer = layer;
+            }
+        }
+
+        void Update()
+        {
+            //transform.position = Camera.main.transform.position + CamPos;
+            //if (nat == null)
+            {
+                //var player = ObjectManager.objectManager.GetMyPlayer();
+                SetObjectLayer(20);
+
+                GetComponent<Camera>().RenderWithShader(shader, null);
+
+                SetObjectLayer(0);
+                /*
+                if (player != null)
+                {
+                    nat = player.GetComponent<NpcAttribute>();
+                }
+                */
+            }
+            /*
+            if (nat != null)
+            {
+                nat.SetShadowLayer();
+                camera.RenderWithShader(shader, null);
+                nat.SetNormalLayer();
+            }
+            */
+
+        }
+    }
+}
